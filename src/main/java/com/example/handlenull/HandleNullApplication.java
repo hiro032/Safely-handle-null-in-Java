f86dad7e375c4160c16a074d1c14e2db7e4cd44d
hiro <kiryuinh32@gmail.com>
2022-10-12 22:15:10 +0900

    init project

---
+++ src/main/java/com/example/handlenull/HandleNullApplication.java
@@ -0,0 +1,13 @@
+package com.example.handlenull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleNullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandleNullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/handlenull/HandleNullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handlenull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleNullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
